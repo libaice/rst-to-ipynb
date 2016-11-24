3315f3a45128c551271ba1853bdf1a5de90e03c4
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-11-24 16:46:02 +0200

    Refactored argument parsing
    - accept rst2ipynb intput output for consistency with other rst2xxx converters
    - implement rst2ipynb -h

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='rst2ipynb',
-    version='0.2.1',
+    version='0.2.2',
     description='A reST to Jupyter notebook converter',
     long_description=long_description,
     url='https://github.com/nthiery/rst-to-ipynb',
